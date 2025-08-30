4075500758242c4e2114781cd245bdd5890d070a
Afsal Thaj <afsal.taj06@gmail.com>
2025-08-30 14:59:35 +0530

    Fix tests

--- golem-ts-sdk/tests/testAgents.ts
+++ golem-ts-sdk/tests/testAgents.ts
@@ -17,8 +17,6 @@
 import { agent, BaseAgent } from '../src';
 import * as Types from './testTypes';
 
-console.log('Loading Agents');
-
 @agent()
 class WeatherAgent extends BaseAgent {
   constructor(readonly testInterfaceType: Types.TestInterfaceType) {
